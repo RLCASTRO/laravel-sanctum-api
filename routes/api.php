0c14f6e12a2b6a14c97bb4948aaddf2e1e156d2d
Rodrigo Castro <castro.rl@gmail.com>
2023-09-01 14:43:39 -0300

    Created a protected routes group for POST/PUT/DELETE routes and left the GET routes as with public access. Generated the AuthController

--- routes/api.php
+++ routes/api.php
@@ -21,22 +21,24 @@ use App\Http\Controllers\ProductController;
 
 //Protected routes
 Route::group(['middleware' => ['auth:sanctum']], function () {
+    // //stores a single product
+    Route::post('/products', [ProductController::class, 'store']);
+    // //stores a single product
+    Route::put('/products/{id}', [ProductController::class, 'update']);
+    // //stores a single product
+    Route::delete('/products/{id}', [ProductController::class, 'destroy']);
 });
 
 
 // Public routes
 //search for products
 Route::get('/products/search/{name}', [ProductController::class, 'search']);
+// //get all products
+Route::get('/products', [ProductController::class, 'index']);
+// //gets a single product
+Route::get('/products/{id}', [ProductController::class, 'show']);
 
-//This will capture all /products routes and call the respective class method for all the CRUD operations
-Route::resource('products', ProductController::class);
 
 //You can separate each route, or if your API has all the basic CRUD routes, you can use resource instead
-// //stores a single product
-// Route::post('/products', [ProductController::class, 'store']);
-
-// //get all products
-// Route::get('/products', [ProductController::class, 'index']);
-
-// //gets a single product
-// Route::get('/products/{id}', [ProductController::class, 'show']);
+//This will capture all /products routes and call the respective class method for all the CRUD operations
+// Route::resource('products', ProductController::class);
